79abafa6b58d751e6cc7e15037d1f5c6a58af4b9
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2023-12-08 01:19:38 -0300

    chore(generate): added minimal ui

--- generate.py
+++ generate.py
@@ -1,10 +1,23 @@
+"""
+    Módulo principal que executa a
+    análise semântica e a geração de código
+"""
+
+import os
+
 from analysers.semantic.semantic import Semantic
 from code_generation.transpiler import Transpiler
 from utils.readers import DataReader as rd
 
 
 def main() -> None:
-    filename = "complete"
+    print("=" * 44)
+    print("\t Visualg Transpiler to C")
+    print("=" * 44)
+    print("Digite o nome do arquivo (sem a extensão .txt) de exemplo"\
+          " da pasta examples/ que deseja gerar")
+    print(f"Arquivos disponíveis: {os.listdir('./examples')}")
+    filename = input(">>> ")
 
     symbols = rd.read_symbols(filename)
 
@@ -14,9 +27,14 @@ def main() -> None:
     trp = Transpiler(symbols, variables)
     output = trp.transpile()
 
+    if not os.path.exists("./output"):
+        os.mkdir("./output")
+
     with open(f"output/{filename}.c", "w") as file:
         file.write(output)
 
+    print("Geração realizada com sucesso!")
+    print(f"O arquivo {filename}.c pode ser acessado na pasta output/")
 
 if __name__ == "__main__":
     main()
